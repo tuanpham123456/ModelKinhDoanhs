c07e3526e19e812a8b96ef8cde4edd270cb81b70
Pham Dac Tuan <phamdactuan@laodong.com.vn>
2022-08-16 09:55:14 +0700

    model first

---
+++ DataAccess/DBContext/AppDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.DBContext
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext() { }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        { }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>(entity =>
+            {
+
+            });
+            modelBuilder.Entity<Ivoice>(entity =>
+            {
+
+            });
+
+        }
+
+        public virtual DbSet<Member> Members { get; set; }
+        public virtual DbSet<Ivoice> Ivoices { get; set; }
+
+    }
+}
---
+++ DataAccess/Member.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Member : BaseEntity
+    {
+        public long ID { get; set; }
+     
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string FacebookId { get; set; }
+
+        public string FacebookAvatarUrl { get; set; }
+
+        public bool IsActived { get; set; }
+
+    }
+}
