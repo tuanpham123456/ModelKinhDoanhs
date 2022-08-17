ed8072aeb66eee2067ff4be1c9709594b93c9296
Vũ Nguyễn Long <nguyenlongvu@laodong.com.vn>
2022-08-17 09:25:09 +0700

    payment_info_cheque, payment_invoice

---
+++ DataAccess/Migrations/20220816080651_payment.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class payment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Ach_ID",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "Payments",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Credit_Car_ID",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Currency_ID",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Deleted",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Is_PreAuth",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Is_Refund",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Method_ID",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Optlock",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Payment_Date",
+                table: "Payments",
+                type: "datetime",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Payment_ID",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Payment_Notes",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Payment_Period",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Payout_ID",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "User_ID",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "amount",
+                table: "Payments",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "attempt",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "result_ID",
+                table: "Payments",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ach_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Credit_Car_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Currency_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Is_PreAuth",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Is_Refund",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Method_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Optlock",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Payment_Date",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Payment_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Payment_Notes",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Payment_Period",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "Payout_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "User_ID",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "amount",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "attempt",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "result_ID",
+                table: "Payments");
+        }
+    }
+}
--- DataAccess/Payment_info_cheque.cs
+++ DataAccess/Payment_info_cheque.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
+    // Kiểm tra thông tin thanh toán
     internal class Payment_info_cheque : BaseEntity
     {
         public long ID { get; set; }
+        public int Paymet_ID { get; set; }
+        public string Bank { get; set; }
+        // số kiểm tra
+        public string Cheque_number { get; set; }
+        // Ngày kiểm tra
+        public DateTime Cheque_date { get; set; }
+        // khóa lựa chọn
+        public int Optlock { get; set; }
     }
 }
