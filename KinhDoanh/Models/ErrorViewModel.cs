eb882188aff737e66c70fe7b7634dc02a7273a0d
Pham Dac Tuan <phamdactuan@laodong.com.vn>
2022-08-17 09:40:29 +0700

    kinhdoanh

---
+++ KinhDoanh/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KinhDoanh.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
